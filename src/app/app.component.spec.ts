81dd52fde651bfd98e1edbee0be1c935d46b8ff6
Abraham Romero <abrahamciclo@gmail.com>
2020-12-16 18:47:47 +0100

    Shopping Cart

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Shopping-Cart'`, () => {
+  it(`should have as title 'my-store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Shopping-Cart');
+    expect(app.title).toEqual('my-store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Shopping-Cart app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('my-store app is running!');
   });
 });
